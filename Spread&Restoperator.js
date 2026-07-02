5a5d831c74f2fef19db2ad35a406742dd376a8fd
bilalpatel813 <bilalpatel78600@gmail.com>
2026-07-02 19:48:36 +0530

    Added Spread&Restoperator.js

---
+++ Spread&Restoperator.js
@@ -0,0 +1,8 @@
+// spread operator
+//arrays : 
+const arr1 = [1,2,3]
+const arr2 = [...arr1,4,5,6]
+const copy =[...arr2]
+console.log("arr1 :" +arr1)
+console.log("arr2 :" +arr2)
+console.log("merge of both array (copy of arr2) :" +copy)
