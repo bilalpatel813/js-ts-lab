d26a3b194ab8f200b3e674c827fc079d6ad8be79
bilalpatel813 <bilalpatel78600@gmail.com>
2026-07-02 19:57:54 +0530

    update Spread&Restoperator.js

--- Spread&Restoperator.js
+++ Spread&Restoperator.js
@@ -6,3 +6,9 @@ const copy =[...arr2]
 console.log("arr1 :" +arr1)
 console.log("arr2 :" +arr2)
 console.log("merge of both array (copy of arr2) :" +copy)
+//objects
+const obj1 ={a:1,b:2};
+const obj2 = {...obj1, c:3}
+const copyobj = {...obj2}
+console.log("obj1 :" +obj1.a,obj1.b)
+console.log("copy object merging both objects :"+copyobj.a,copyobj.b,copyobj.c)
