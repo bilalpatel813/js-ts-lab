b3980e83e87eae4be83842bf8e4cc80025647c38
bilalpatel813 <bilalpatel78600@gmail.com>
2026-07-04 18:10:59 +0530

    updated DOM

--- DOM/index.js
+++ DOM/index.js
@@ -1,3 +1,21 @@
-let el = document.getElementById("head");
+let el = document.getElementById("head"); // elements I'd by getElement 
 console.log(el)
-console.log("hello")
+let c = document.querySelector(".myClass") // class by querySelector 
+let i = document.querySelector("#head") //id by querySelector 
+let t = document.querySelector("p") // selected tag by querySelector 
+console.log(i.textContent)// textContent give text inside class, element or tag. 
+console.log(c.textContent);
+console.log(t.textContent)
+console.log("hello")// hello  by Bilal dev! 
+const paragraphs = document.querySelectorAll("p");//selected all same tag in file 
+console.log(paragraphs[0].textContent); //getting  tag like  array indexing  
+console.log(paragraphs);
+let divs = document.getElementsByTagName("div");
+console.log(divs)//gives html collection like and array 
+// changing html test through js on DOM
+el.innerHTML = "<b>Bold</b>";
+t.textContent = " changed the Para element/tag"
+// changing CSS 
+el.style.color ="blue"
+el.style.backgroundColor= "black"
+el.style.fontSize = "24px"
