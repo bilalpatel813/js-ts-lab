24fe721f379223bb4d9022a535aa36431a87442c
bilalpatel813 <patelbilal8137@gmail.com>
2026-08-19 20:00:29 +0530

    learned loading screens in web

--- nextjs/next-app/app/product/[productId]/page.tsx
+++ nextjs/next-app/app/product/[productId]/page.tsx
@@ -1,5 +1,10 @@
 
 export default async function productList({params}:{params:Promise<{productId:string}>;}) {
+  await new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve("delayed on purpose to ragebait User!! have fun..")
+    },5000)
+  })
   const productId = (await params).productId;
   
   return(<>
