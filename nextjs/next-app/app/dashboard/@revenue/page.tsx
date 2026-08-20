499ca81de1f508356dd6d3837628cb18caf23558
bilalpatel813 <patelbilal8137@gmail.com>
2026-08-21 01:35:04 +0530

    parallel routes

---
+++ nextjs/next-app/app/dashboard/@revenue/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/card";
+import Link from "next/link";
+
+export default function Revenue() {
+  return (
+    <Card>
+      <div>Revenue metrics</div>
+      <div>
+      </div>
+    </Card>
+  );
+}
---
+++ nextjs/next-app/app/dashboard/@users/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/card";
+import Link from "next/link";
+
+export default function Users() {
+  return (
+    <Card>
+      <div>Users</div>
+      <div>
+      </div>
+    </Card>
+  );
+}
